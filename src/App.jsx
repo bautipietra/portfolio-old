9b5efcb85d54d48700532bb28647d19596eaa66b
Bautista Pietraroia <bautistapietraroia@gmail.com>
2022-11-29 00:12:11 -0300

    remove hash from url

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import './App.scss'
 import Nav from './components/Nav/Nav'
 import Contact from './components/Contact/Contact'
 import Projects from './components/Projects/Projects'
+import removeUrlHash from 'remove-url-hash';
 
 function App() {
+
+  removeUrlHash()
 
   return (
     <div className="App">
